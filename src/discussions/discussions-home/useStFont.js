adf195a284e22948b32a1091135d433500207686
Khaled <info@khaledr.ir>
2023-11-27 11:01:29 +0330

    fix: fix fa lang code

--- src/discussions/discussions-home/useStFont.js
+++ src/discussions/discussions-home/useStFont.js
@@ -6,7 +6,7 @@ const useSetFont = () => {
   useEffect(() => {
     const setFont = () => {
       const body = document.querySelector('body');
-      if (getLocale() === 'fa') {
+      if (getLocale() === 'fa' || getLocale() === 'fa-ir') {
         body.className = 'lang_fa';
       } else if (getLocale() === 'ar') {
         body.className = 'lang_ar';
